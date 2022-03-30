de8e413ca1a59469236e7b67a217529a777b31f9
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-03-30 02:02:24 -0300

    criando função de comparaçao

--- script.js
+++ script.js
@@ -5,23 +5,58 @@ let priceKm = document.getElementById('pKm')
 let priceFuel = document.getElementById('vFuel')
 let txtConsumptionMedia = document.getElementById('consumptionMedia')
 let txtPriceKm = document.getElementById('priceKm')
+const listCars = [
+{
+     name: "Honda fit", consumption: 13 
+}, 
+{
+    name: "Toyota Corolla", consumption: 8
+}, 
+{
+    name:"Gol", consumption: 12
+},
+{
+    name:"Focus", consumption: 7
+}]
 
-// funcção principal, chama outras funções
+
+
+// função principal, chama outras funções
 function calculate() {
     calculateMediaConsumption()
     calculatePriceKm()
+    modelsCars()
 }
 // função calcula media de consumo
 function calculateMediaConsumption() {
     let consumptionMedia = (Number(kmf.value) - Number(kmi.value)) / Number(qtdLiters.value)
-    txtConsumptionMedia.innerHTML = `O consumo médio é de ${consumptionMedia} km/l.`
-    return consumptionMedia.toFixed(2)
+    txtConsumptionMedia.innerHTML = `O consumo médio é de ${consumptionMedia.toFixed(2)} km/l.`
+    return consumptionMedia
 }
 // função calcula preço por km rodado
-function calculatePriceKm(){
+function calculatePriceKm() {
     let kmr = Number(kmf.value) - Number(kmi.value)
-    let vTotalStocked = Number(qtdLiters.value) * Number(priceFuel.value)
-    let priceKmr = vTotalStocked / kmr 
-    txtPriceKm.innerHTML = `O preço por km rodado é de r$ ${priceKmr}`
-    return priceKm.toFixed(2)
+    let vTotalStocked = Number(qtdLiters.value) * Number(priceKm.value)
+    let priceKmr = vTotalStocked / kmr
+    txtPriceKm.innerHTML = `O preço por km rodado é de r$ ${priceKmr.toFixed(2)}`
+    return priceKmr
 }
+// escolhe modelo do carro
+function modelsCars() {
+    let select = document.getElementById('Cars')
+    let value = select.options[select.selectedIndex].value
+    
+    if (select.value == listCars.name || consumptionMedia == listCars.consumption) {
+        console.log('O consumo está dentro do esperado')
+        // comparar media de consumo
+    } else if (select.value == listCars.name || consumptionMedia > listCars.consumption) {
+        console.log('O consumo está acima da media do fabricante')
+    } else if (select.value == listCars.name || consumptionMedia < listCars.consumption) {
+        console.log('O consumo está abaixo do fabricante')
+    }
+    
+}
+
+
+
+
